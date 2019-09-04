bb012bd958c68ec2d74eba085b83859a4fe67ee9
elia migliore <migliorelia@gmail.com>
2019-09-04 09:54:25 +0200

    Update start.c
    Added the hello world string.
    Because the machine output is I/O dependent, this doesn't work without ifdef for platform and hardware dependent code

--- ex1-nostdlib/start.c
+++ ex1-nostdlib/start.c
@@ -3,15 +3,50 @@
 /*-----------------------------------------*/
 // to be compiled with `gcc -o start -nostdlib start.c`
 
-int main();
+/* this codes works on 64 bit linux. */
+
+#define SYSCALL_EXIT 60
+#define SYSCALL_WRITE 1
+
+int main(void);
+void sys_exit(int error_code);
+int sys_write(unsigned fd, const char *buf, unsigned count);
 
 // entry point of the program
 void _start(){
 
 	int ret = main();
+	sys_exit(12);
 }
 
 int main(){
+	const char hwText[] = "Hello world!\n";
+	sys_write(1, hwText, sizeof(hwText));
 	return 0;
 }
 
+void sys_exit(int error_code)
+{
+    asm volatile
+    (
+        "syscall"
+        : 
+        : "a"(SYSCALL_EXIT), "D"(error_code)
+        : "rcx", "r11", "memory"
+    );
+}
+
+int sys_write(unsigned fd, const char *buf, unsigned count)
+{
+    unsigned ret;
+
+    asm volatile
+    (
+        "syscall"
+        : "=a"(ret)
+        : "a"(SYSCALL_WRITE), "D"(fd), "S"(buf), "d"(count)
+        : "rcx", "r11", "memory"
+    );
+
+    return ret;
+}
